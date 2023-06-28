ba268825daac5628fba9b5ee2ef02f3dd6af1135
vitor98rods <126877198+vitor98rods@users.noreply.github.com>
2023-06-28 11:17:07 -0300

    Create ex05.go

---
+++ ex05.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+// Crie um Array bidimensional de inteiros
+//com 3 linhas e 2 colunas.
+//Solicite ao usuário que informe os
+//valores de cada elemento da matriz.
+//Em seguida, imprima a matriz resultante.
+
+func main() {
+	var matriz [3][2]int
+
+	for linha := 0; linha < len(matriz); linha++ {
+		for coluna := 0; coluna < len(matriz[linha]); coluna++ {
+			var x int
+			fmt.Println("Digite o número da linha ", linha, " e da coluna ", coluna)
+			fmt.Scan(&x)
+			matriz[linha][coluna] = x
+		}
+	}
+
+	fmt.Println(matriz)
+}
